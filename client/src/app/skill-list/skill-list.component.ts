f14eefb523a0a0d6cb3c0e2738c385e78d2e2f20
Christine Johnson <christine.johnson@ilendx.com>
2020-01-27 20:53:31 -0600

    committing so that in case I mess up this next bit I can roll back

--- client/src/app/skill-list/skill-list.component.ts
+++ client/src/app/skill-list/skill-list.component.ts
@@ -4,61 +4,66 @@ import { SkillService } from '../shared/skill/skill.service';
 import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
 
 @Component({
-  selector: 'app-skill-list',
+  selector: 'skill-list',
   templateUrl: './skill-list.component.html',
   styleUrls: ['./skill-list.component.css']
 })
+
 export class SkillListComponent implements OnInit {
 
-  headers = ["Proficient", "Bonus", "Skill", "Modifier"];
+  skills: Array<any>;
 
-  rows = [
-    {
-      "Proficient": true,
-      "Bonus": "-1",
-      "Skill": "Acrobatics",
-      "Modifier": "Dexterity"
-    },
-    {
-      "Proficient": false,
-      "Bonus": "+2",
-      "Skill": "Animal Handling",
-      "Modifier": "Wisdom"
-    },
-    {
-      "Proficient": false,
-      "Bonus": "+2",
-      "Skill": "Arcana",
-      "Modifier": "Intelligence"
-    },
-    {
-      "Proficient": false,
-      "Bonus": "+5",
-      "Skill": "Athletics",
-      "Modifier": "Strength"
-    },
-    {
-      "Proficient": false,
-      "Bonus": "+2",
-      "Skill": "Deception",
-      "Modifier": "Wisdom"
-    },
-    {
-      "Proficient": false,
-      "Bonus": "-1",
-      "Skill": "History",
-      "Modifier": "Intelligence"
-    }
-  ]
+  constructor(private skillService: SkillService) { }
 
-// skills: Array<any>;
+  ngOnInit() {
+    this.skillService.getAllSkills().subscribe(data => {
+      this.skills = data
+    });    
 
-// constructor(private skillService: SkillService) { }
-
-//   ngOnInit() {
-//     this.skillService.getAllSkills().subscribe(data => {
-//       this.skills = data
-//     });
-//   }
+  }
 
 }
+// export class SkillListComponent implements OnInit {
+
+//   headers = ["Proficient", "Bonus", "Skill", "Modifier"];
+
+//   rows = [
+//     {
+//       "Proficient": true,
+//       "Bonus": "-1",
+//       "Skill": "Acrobatics",
+//       "Modifier": "Dexterity"
+//     },
+//     {
+//       "Proficient": false,
+//       "Bonus": "+2",
+//       "Skill": "Animal Handling",
+//       "Modifier": "Wisdom"
+//     },
+//     {
+//       "Proficient": false,
+//       "Bonus": "+2",
+//       "Skill": "Arcana",
+//       "Modifier": "Intelligence"
+//     },
+//     {
+//       "Proficient": false,
+//       "Bonus": "+5",
+//       "Skill": "Athletics",
+//       "Modifier": "Strength"
+//     },
+//     {
+//       "Proficient": false,
+//       "Bonus": "+2",
+//       "Skill": "Deception",
+//       "Modifier": "Wisdom"
+//     },
+//     {
+//       "Proficient": false,
+//       "Bonus": "-1",
+//       "Skill": "History",
+//       "Modifier": "Intelligence"
+//     }
+//   ]
+
+
